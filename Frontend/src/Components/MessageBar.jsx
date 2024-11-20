e83f52972a3c130193a96183fdd1de9a9a143bb4
mr9 asif <asifalibd002@gmail.com>
2024-11-20 23:30:32 +0600

    added msg section

--- Frontend/src/Components/MessageBar.jsx
+++ Frontend/src/Components/MessageBar.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { AuthContext } from '../ContextApi/Context';
 import useConversation from '../zustand/useConversation';
 import Loader from './Loader';
 
@@ -7,6 +8,14 @@ const MessageBar = () => {
     const {selectedConversation, setMessages, messages}=useConversation();
     // console.log(selectedConversation)
     const [loading, setLoading] = useState(false);
+    const lastMessageRef = useRef();
+    const {user}=useContext(AuthContext);
+
+    useEffect(()=>{
+       setTimeout(()=>{
+            lastMessageRef?.current?.scrollIntoView({behavior:"smooth"})
+       },1000)
+    },[messages])
 
     useEffect(()=>{
          setLoading(true);
@@ -31,16 +40,34 @@ const MessageBar = () => {
     },[selectedConversation, setMessages])
     console.log(messages)
     return (
-        <div className='h-full w-full '>
-            <div className='flex items-center h-[100px] px-6 justify-start bg-[#B7B7B7] gap-2'>
+        <div className='flex flex-col   h-screen w-full '>
+            <div className='flex items-center h-[100px] px-6 justify-start bg-gray-300 gap-2'>
                 <img className='w-[50px] rounded-[40px]' src={selectedConversation?.pic} alt="" />
                 <h1 className='text-[16px] text-white font-semibold'>{selectedConversation?.name}</h1>
             </div>
 
-            <div className='flex-1 overflow-auto'>
+            <div className='flex-1 h-full overflow-auto bg-gray-500'>
                   {loading && <Loader></Loader>}
                   {!loading && messages?.length == 0 && ( <div>send a msg to chat</div>) }
-                
+                    
+                   {!loading && messages?.length > 0 && messages?.map((message) => (
+                <div className='text-white' key={message?._id} ref={lastMessageRef}>
+                  <div className={`chat ${message.senderId === user._id ? 'flex justify-end' : 'flex justify-start'}`}>
+                    <div className='flex flex-col'></div>
+                    <div className={`chat-bubble p-2 `}>
+                      <p className={`p-2 rounded-md ${message.senderId === user._id ? 'bg-sky-600' : 'bg-gray-400'
+
+                    }`}>{message?.message}</p>
+                       <div className=" text-[13px] flex items-center  gap-2 font-semibold  opacity-80">
+                      <h1>{new Date(message?.createdAt).toLocaleDateString('en-IN')}</h1>
+                      <p> {new Date(message?.createdAt).toLocaleTimeString('en-IN', { hour: 'numeric', minute:
+                         'numeric' })}</p>
+                    </div>
+                    </div>
+                   
+                  </div>
+                </div>
+              ))}
             </div>
         </div>
     );
