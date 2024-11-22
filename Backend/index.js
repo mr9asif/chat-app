6000f8809040c58f5991b6a4670e9598ad21e614
mr9 asif <asifalibd002@gmail.com>
2024-11-22 21:29:41 +0600

    finish

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ const userRoutes = require('./routes/userRoutes');
 const msgRouter = require('./routes/msgRoutes')
 const User = require("./Models/user");
 const generateToken = require("./congig/jwtToken");
+const path = require('path');
 require("dotenv").config();
 const cookieParser = require('cookie-parser');
 const isLogin = require("./MiddleWare/Islogin");
@@ -12,9 +13,9 @@ const { logOut } = require("./controller/userControllers");
 const { server, app } = require("./Socket/socket");
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4001;
 
-
+const __dirname = path.resolve();
 
 // middle man
 app.use(express.json());
@@ -31,6 +32,10 @@ app.use("/api/logout", logOut)
 app.use('/', isLogin)
 app.get('/m', (req, res)=>{
  res.send({message:"hi"})
+})
+app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
 })
 
 server.listen(port, ()=>{
